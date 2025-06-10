c3d5f6262198985596254de84f79fd58ebdfbd08
Anas El Mouden <anaselmouden99@gmail.com>
2025-06-10 13:33:47 +0200

    fix

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,6 +1,4 @@
 'use client';
-
-import { useEffect } from 'react';
 import { useAnalyticsEvents } from './providers/AnalyticsEventsProvider'; // Adjust path as needed
 import EventCard from './EventCard'; // Adjust path as needed
 
